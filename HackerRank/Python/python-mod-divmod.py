274f3afa40aa418c7bade7397629bde46e1bd172
Sahil Khose <sahilkhose18@gmail.com>
2020-08-25 09:27:47 +0530

    Add python basic qns

---
+++ HackerRank/Python/python-mod-divmod.py
@@ -0,0 +1,3 @@
+a, b = (int(input()) for _ in range(2))
+[print(x) for x in divmod(a, b)]
+print(divmod(a, b))
---
+++ HackerRank/Python/python-power-mod-power.py
@@ -0,0 +1,3 @@
+a, b, m  = float(input()), float(input()), int(input())
+print(int(pow(a, b)))
+print(pow(int(a), abs(int(b)), m))
